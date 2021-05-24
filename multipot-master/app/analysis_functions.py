162b94bd4d7efc069f9b8c369a204854867e5d74
Lija G <lijamgopal@gmail.com>
2021-05-24 21:37:40 +0530

    Refs # removed drupal and wordpress contents

--- multipot-master/app/analysis_functions.py
+++ multipot-master/app/analysis_functions.py
@@ -35,33 +35,6 @@ def ip_addresses_top(x, days):
         return data_top_ip
 
 
-def wordpress_wp_login_usernames_password_tries_top(x):
-    data_form = db.session.query(models.Request.form).filter_by(endpoint="wp_login", method="POST").all()
-    data_usernames = [i[0]["log"] for i in data_form]
-    data_pwds = [i[0]["pwd"] for i in data_form]
-    data_usernames_tries_counted = Counter(data_usernames).most_common(x)
-    data_pwds_tries_counted = Counter(data_pwds).most_common(x)
-    return data_usernames_tries_counted, data_pwds_tries_counted
-
-
-def wordpress_wp_login_password_tries_count():
-    data_count = db.session.query(models.Request.form).filter_by(endpoint="wp_login", method="POST").count()
-    return data_count
-
-
-def wordpress_wp_login_password_tries_last_24h_count():
-    data_count = (
-        db.session.query(models.Request)
-        .filter(
-            models.Request.timestamp > (datetime.utcnow() - timedelta(days=1)),
-            models.Request.endpoint == "wp_login",
-            models.Request.method == "POST",
-        )
-        .count()
-    )
-    return data_count
-
-
 def stored_requests_count():
     data_count = db.session.query(models.Request).count()
     return data_count
@@ -150,82 +123,6 @@ def requests_last_x_days_chart(x):
     return days, requests
 
 
-"""
-# clean (defusedxml) version of the wordpress_xmlrcp_username_password_tries_top function below, however very slow
-def wordpress_xmlrcp_username_password_tries_top(x):
-    usernames = []
-    passwords = []
-    data_form = db.session.query(models.Request.form).filter_by(endpoint='wp_xmlrpc', method='POST').all()
-    for a in data_form:
-        key = list(a[0].keys())[0]
-        value = list(a[0].values())[0]
-        root = fromstring(key+"="+value)
-        username_password = root.findall("./params/param/value/array/data/value/struct/member/value/array/data/value/array/data/value/string")
-        if len(username_password) == 2:
-            usernames.append(username_password[0].text)
-            passwords.append(username_password[1].text)
-        else:
-            continue
-    usernames = Counter(usernames).most_common(x)
-    passwords = Counter(passwords).most_common(x)
-    return usernames, passwords
-"""
-
-
-# not ideal as it would fail to account for newlines or spaces in the form string, etc.
-# TODO: evaluate sane methods to parse xml. 'defusedxml' is too slow
-# ? content = re.sub('\s+', ' ', content)  # condense all whitespace
-# ? content = re.sub('[^A-Za-z ]+', '', content)  # remove non-alpha chars
-def wordpress_xmlrpc_username_password_tries_top(x):
-    usernames = []
-    passwords = []
-    data_form = db.session.query(models.Request.form).filter_by(endpoint="wp_xmlrpc", method="POST").all()
-    for a in data_form:
-        if list(a[0].values()):
-            value = list(a[0].values())[0]
-            try:
-                b = value.split("<member><name>params</name><value><array><data><value><array><data><value><string>")
-                c = b[1].split("</string></value></data></array></value></data></array></value></member>")
-                d = c[0].split("</string></value><value><string>")
-                usernames.append(d[0])
-                passwords.append(d[1])
-            except:
-                continue
-    usernames = Counter(usernames).most_common(x)
-    passwords = Counter(passwords).most_common(x)
-    return usernames, passwords
-
-
-def wordpress_xmlrpc_password_tries_count():
-    counter = 0
-    data_form = db.session.query(models.Request.form).filter_by(endpoint="wp_xmlrpc", method="POST").all()
-    for a in data_form:
-        if list(a[0].values()):
-            value = list(a[0].values())[0]
-            if any(x in value for x in ["getUserBlogs", "getUsersBlogs"]):
-                counter = counter + 1
-    return counter
-
-
-def wordpress_xmlrpc_password_tries_last_24h_count():
-    counter = 0
-    data_form = (
-        db.session.query(models.Request.form)
-        .filter(
-            models.Request.timestamp > (datetime.utcnow() - timedelta(days=1)),
-            models.Request.endpoint == "wp_xmlrpc",
-            models.Request.method == "POST",
-        )
-        .all()
-    )
-    for a in data_form:
-        if list(a[0].values()):
-            value = list(a[0].values())[0]
-            if any(x in value for x in ["getUserBlogs", "getUsersBlogs"]):
-                counter = counter + 1
-    return counter
-
-
 def gather_ip_geo_whois(ip):
     r = requests.get(
         "http://ip-api.com/json/"
@@ -243,42 +140,3 @@ def requests_all_days_chart(ip):
     days = [i[0].isoformat() for i in days_counted_sorted]
     requests = [i[1] for i in days_counted_sorted]
     return days, requests
-
-
-"""
-Drupal analysis
-"""
-
-
-def drupal_username_password_tries_top(x):
-    usernames = []
-    passwords = []
-    data_form = db.session.query(models.Request.form).filter_by(endpoint="drupal8_login", method="POST").all()
-    for a in data_form:
-        try:
-            usernames.append(a[0]["name"])
-            passwords.append(a[0]["pass"])
-        except:
-            continue
-    usernames = Counter(usernames).most_common(x)
-    passwords = Counter(passwords).most_common(x)
-    return usernames, passwords
-
-
-def drupal_password_tries_count():
-    data_counter = db.session.query(models.Request).filter_by(endpoint="drupal8_login", method="POST").count()
-    # data_counter = db.session.query(models.Request).filter(or_(models.Request.endpoint=='drupal_login', models.Request.endpoint=='drupal8_login'), models.Request.method=='POST').count()
-    return data_counter
-
-
-def drupal_password_tries_last_24h_count():
-    data_counter = (
-        db.session.query(models.Request)
-        .filter(
-            models.Request.timestamp > (datetime.utcnow() - timedelta(days=1)),
-            models.Request.endpoint == "drupal8_login",
-            models.Request.method == "POST",
-        )
-        .count()
-    )
-    return data_counter
